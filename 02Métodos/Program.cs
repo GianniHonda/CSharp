04ffcee7bc34efd6c93a7a8eb548fd1e2a67a383
Gianni Stephanie Carreira Honda <90318510+GianniHonda@users.noreply.github.com>
2023-09-22 07:19:12 +0900

    Second Exercise - Methods
    In this exercise, I followed the logic of the previous one, however, with the creation of methods.

---
+++ 02Métodos/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _02Métodos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa obj = new Pessoa();
+            obj.apresentar("Ralf", 30);
+        }
+    }
+}
