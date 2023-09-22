f97aed8726690b1cee3e240cffb82d7b5e52091b
Gianni Stephanie Carreira Honda <90318510+GianniHonda@users.noreply.github.com>
2023-09-22 11:40:45 +0900

    Exercise of Inheritance
    In this exercise I worked with the concept of inheritance, bringing the characteristics of a person, calling two distinct classes but correlated by inheritance.

---
+++ 10Heranca/Pessoa.cs
@@ -0,0 +1,15 @@
+using System;
+
+class Pessoa
+{
+    //Atributos
+    protected string nome;
+    protected int idade;
+
+    //Método
+    protected void mensagemPessoa()
+    {
+        Console.WriteLine("Nome "+nome);
+        Console.WriteLine("Idade "+idade);
+    }
+}
---
+++ 10Heranca/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _08GetSet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Colaborador obj = new Colaborador("Ralf", 30, 2000);
+        }
+    }
+}
