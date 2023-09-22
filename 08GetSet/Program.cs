7a2a1bb50838364c1c13f69b10627e6385364aa6
Gianni Stephanie Carreira Honda <90318510+GianniHonda@users.noreply.github.com>
2023-09-22 10:59:21 +0900

    Get and Set Exercise
    Same concept as the previous exercise, however, using the Get and Set methods.

---
+++ 08GetSet/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _08GetSet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          Pessoa p = new Pessoa();
+          p.Nome = "Ralf";
+          Console.WriteLine(p.Nome);
+
+        }
+    }
+}
