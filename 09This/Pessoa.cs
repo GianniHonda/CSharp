ce809886eca16c6d6dd4c1de05cafc40fc4ce321
Gianni Stephanie Carreira Honda <90318510+GianniHonda@users.noreply.github.com>
2023-09-22 11:17:35 +0900

    Exercise "This"
    Same basis as the previous exercise, though, using "This" to call the method and parameter.

---
+++ 09This/Pessoa.cs
@@ -0,0 +1,14 @@
+using System;
+
+class Pessoa
+{
+    //Atributo
+    private string nome = "Tatiana";
+
+    //Construtor
+    public Pessoa(string nome)
+    {
+        Console.WriteLine(nome);
+        Console.WriteLine(this.nome);
+    }
+}
---
+++ 09This/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _08GetSet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         Pessoa p = new Pessoa("Ralf");
+        }
+    }
+}
