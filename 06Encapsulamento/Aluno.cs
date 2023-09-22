e17e49aa53e09d11c20bfc1755937d5e24c52405
Gianni Stephanie Carreira Honda <90318510+GianniHonda@users.noreply.github.com>
2023-09-22 10:26:34 +0900

    Anchoring exercise
    This exercise uses encapsulating the attributes and methods of a class, switching between private and public. The calculation of the average of a student is performed, giving access only to the "message" method.

---
+++ 06Encapsulamento/Aluno.cs
@@ -0,0 +1,25 @@
+using System;
+
+class Aluno 
+{
+    // Atributos
+    private double nota1, nota2;
+
+    //Média
+    private double media()
+    {
+        return (nota1+nota2)/2;
+    }
+
+    //mensagem
+    public void mensagem()
+    {
+        Console.WriteLine("Informe a primeira nota");
+        nota1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Informe a segunda nota");
+        nota2 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("A média é "+media());
+    }
+}
