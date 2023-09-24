a8dc0d22f363b35bcf12ed01b88eab0b41103c25
Gianni Stephanie Carreira Honda <90318510+GianniHonda@users.noreply.github.com>
2023-09-25 07:21:21 +0900

    Static attribute and method exercise
    In this exercise I created a "name" method and a static sum attribute.

---
+++ 12Estatico/Exemplo.cs
@@ -0,0 +1,13 @@
+using System;
+
+class Exemplo
+{
+    //Atributo estático
+    public static string nome = "Ralf";
+
+    //Método estático
+    public static void soma(int n1, int n2)
+    {
+        Console.WriteLine(n1+n2);
+    }
+}
---
+++ 12Estatico/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _08GetSet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         Exemplo.soma(3,7);
+
+         Console.WriteLine("Olá "+Exemplo.nome);
+        }
+    }
+}
