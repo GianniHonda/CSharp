679355d2551108ed6c205798befdddf739de2689
Gianni Stephanie Carreira Honda <90318510+GianniHonda@users.noreply.github.com>
2023-09-26 07:19:21 +0900

    Testing variables

---
+++ Aula03/Aula03.cs
@@ -0,0 +1,14 @@
+using System;
+
+class Aula03
+{
+    static void Main()
+    {
+        int num1, num2, res;
+        num1 = 10;
+        num2 = 2;
+        res = num1 * num2;
+
+        Console.WriteLine("A multiplicação de "+ num1 +" mais "+ num2 + " é igual a: "+ res);
+    }
+}
