fb659d73480ed1ff0c115efd09b289b31af37cd5
Gianni Stephanie Carreira Honda <90318510+GianniHonda@users.noreply.github.com>
2023-09-25 11:57:54 +0900

    C# Standard Language Test in the program

---
+++ Curso_cs/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class aula01
+{
+    static void Main()
+    {
+        Console.Write("Hello World...\n");
+        Console.Write("CFB Cursos");
+        Console.ReadLine();
+    }
+}
+
