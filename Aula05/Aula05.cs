79c36714ac66474e27d2057a0bec0ebad01cc8ad
Gianni Stephanie Carreira Honda <90318510+GianniHonda@users.noreply.github.com>
2023-09-26 08:01:43 +0900

    Testing operators and operations

---
+++ Aula05/Aula05.cs
@@ -0,0 +1,13 @@
+using System;
+
+class Aula05
+{
+    static void Main()
+    {
+        // & = AND / E
+        // | = OR / OU
+        bool res = (5>3)|(10<5);
+
+        Console.WriteLine(res);
+    }
+}
