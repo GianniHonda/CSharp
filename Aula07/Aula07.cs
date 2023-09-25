569d9fda47bcaac610047a5668744910169094dc
Gianni Stephanie Carreira Honda <90318510+GianniHonda@users.noreply.github.com>
2023-09-26 08:31:32 +0900

    Training Constants

---
+++ Aula07/Aula07.cs
@@ -0,0 +1,12 @@
+using System;
+
+class Aula07
+{
+    static void Main()
+    {
+        const string canal = "CFB Cursos";
+        const double pi = 3.1415;
+
+        Console.WriteLine("Canal {0}\nPi: {1}",canal,pi);
+    }
+}
