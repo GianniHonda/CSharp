3584f77081e5e73f7c60126c522794f76eed8c05
Gianni Stephanie Carreira Honda <90318510+GianniHonda@users.noreply.github.com>
2023-09-26 09:33:21 +0900

    Training Type Conversions (typecast)

---
+++ Aula11/Aula11.cs
@@ -0,0 +1,15 @@
+using System;
+
+class Aula11
+{
+    static void Main()
+    {
+        int vInt = 10;
+        short vShort = (short)vInt;
+        
+        /*float n1 = 10.5f;
+        int n2 = (int)n1;//type cast*/
+
+        Console.WriteLine(vShort);
+    }
+}
