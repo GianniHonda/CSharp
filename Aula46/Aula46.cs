3a535f6d84badff504a37c83b657305e219a31f0
Gianni Stephanie Carreira Honda <90318510+GianniHonda@users.noreply.github.com>
2023-09-29 10:50:18 +0900

    Methods that return objects

---
+++ Aula46/Aula46.cs
@@ -0,0 +1,41 @@
+using System;
+
+class Galinha{
+    private string nomeGalinha;
+    private int numOvo;
+    public Galinha(string nomeGalinha){
+        this.nomeGalinha=nomeGalinha;
+        numOvo=0;
+    }
+    public Ovo botar(){
+        numOvo++;
+        return new Ovo(numOvo,nomeGalinha);
+    }
+}
+
+class Ovo{
+    private int numOvo;
+    private string minhaGalinha;
+    public Ovo(int numOvo,string minhaGalinha){
+        this.numOvo=numOvo;
+        this.minhaGalinha=minhaGalinha;
+        Console.WriteLine("Ovo criado:{0} - {1}",this.numOvo,this.minhaGalinha);
+    }
+}
+
+class Aula46{
+    static void Main(){
+        Galinha g1=new Galinha("Beneditasgildimar");
+        Galinha g2=new Galinha("Felizbertaniazilda");
+        Galinha g3=new Galinha("Marisgertrudeszela");
+
+        g1.botar();
+        g1.botar();
+        g1.botar();
+
+        g2.botar();
+
+        g3.botar();
+        g3.botar();
+    }
+}
