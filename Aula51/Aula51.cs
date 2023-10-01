5745abec418f357059343f61c4b58b6f94150593
Gianni Stephanie Carreira Honda <90318510+GianniHonda@users.noreply.github.com>
2023-10-02 08:49:39 +0900

    Program input arguments

---
+++ Aula51/Aula51.cs
@@ -0,0 +1,16 @@
+using System;
+
+class Aula51{
+    static void Main(string[] args){
+        int res = 0;
+        if(args.Length >0){
+            Console.WriteLine("Qtde de argumentos {0}",args.Length);
+            for(int i=0; i<args.Length; i++){
+                res+=Int32.Parse(args[i]);
+            }
+            Console.WriteLine("Soma:{0}",res);
+        }else{
+            Console.WriteLine("Não foram passados argumentos");
+        }
+    }
+}
