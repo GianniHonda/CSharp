f60a44c5e19fab1e80a3b6c8529e426d8776ed94
Gianni Stephanie Carreira Honda <90318510+GianniHonda@users.noreply.github.com>
2023-10-04 09:07:10 +0900

    Textbox Component
    I created a program where the user adds several vehicles through the text box, he can also clear all content and start over.

---
+++ Componentes/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Componentes
+{
+    public partial class F_Principal : System.Windows.Forms.Form
+    {
+        public F_Principal()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_adicionar_Click(object sender, EventArgs e)
+        {
+            if(tb_veiculo.Text == "")
+            {
+                MessageBox.Show("Digite um veículo");
+                tb_veiculo.Focus();
+                return;
+            }
+            tb_listaVeiculos.Text += tb_veiculo.Text + ", ";
+            //tb_listaVeiculos.Text = tb_listaVeiculos.Text + tb_veiculo.Text;
+
+            tb_veiculo.Clear();
+            tb_veiculo.Focus();
+        }
+
+        private void btn_limpar_Click(object sender, EventArgs e)
+        {
+            tb_listaVeiculos.Clear();
+            tb_veiculo.Clear();
+            tb_veiculo.Focus();
+        }
+    }
+}
