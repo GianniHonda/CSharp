3663b717f7c99ffa7807f328d16bc057f61efb8a
Gianni Stephanie Carreira Honda <90318510+GianniHonda@users.noreply.github.com>
2023-10-06 07:22:20 +0900

    Continued (using the CheckBox)

---
+++ Componentes/Componentes/F_CheckBox.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace Componentes
+{
+    partial class F_CheckBox
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cb_carro = new System.Windows.Forms.CheckBox();
+            this.cb_aviao = new System.Windows.Forms.CheckBox();
+            this.cb_navio = new System.Windows.Forms.CheckBox();
+            this.cb_onibus = new System.Windows.Forms.CheckBox();
+            this.btn_transportesMarcados = new System.Windows.Forms.Button();
+            this.cb_patinete = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // cb_carro
+            // 
+            this.cb_carro.AutoSize = true;
+            this.cb_carro.Location = new System.Drawing.Point(12, 12);
+            this.cb_carro.Name = "cb_carro";
+            this.cb_carro.Size = new System.Drawing.Size(51, 17);
+            this.cb_carro.TabIndex = 0;
+            this.cb_carro.Text = "Carro";
+            this.cb_carro.UseVisualStyleBackColor = true;
+            // 
+            // cb_aviao
+            // 
+            this.cb_aviao.AutoSize = true;
+            this.cb_aviao.Location = new System.Drawing.Point(12, 36);
+            this.cb_aviao.Name = "cb_aviao";
+            this.cb_aviao.Size = new System.Drawing.Size(53, 17);
+            this.cb_aviao.TabIndex = 1;
+            this.cb_aviao.Text = "Avião";
+            this.cb_aviao.UseVisualStyleBackColor = true;
+            // 
+            // cb_navio
+            // 
+            this.cb_navio.AutoSize = true;
+            this.cb_navio.Location = new System.Drawing.Point(12, 60);
+            this.cb_navio.Name = "cb_navio";
+            this.cb_navio.Size = new System.Drawing.Size(54, 17);
+            this.cb_navio.TabIndex = 2;
+            this.cb_navio.Text = "Navio";
+            this.cb_navio.UseVisualStyleBackColor = true;
+            // 
+            // cb_onibus
+            // 
+            this.cb_onibus.AutoSize = true;
+            this.cb_onibus.Location = new System.Drawing.Point(12, 84);
+            this.cb_onibus.Name = "cb_onibus";
+            this.cb_onibus.Size = new System.Drawing.Size(59, 17);
+            this.cb_onibus.TabIndex = 3;
+            this.cb_onibus.Text = "Ônibus";
+            this.cb_onibus.UseVisualStyleBackColor = true;
+            // 
+            // btn_transportesMarcados
+            // 
+            this.btn_transportesMarcados.Location = new System.Drawing.Point(93, 8);
+            this.btn_transportesMarcados.Name = "btn_transportesMarcados";
+            this.btn_transportesMarcados.Size = new System.Drawing.Size(230, 23);
+            this.btn_transportesMarcados.TabIndex = 4;
+            this.btn_transportesMarcados.Text = "Transportes marcados";
+            this.btn_transportesMarcados.UseVisualStyleBackColor = true;
+            this.btn_transportesMarcados.Click += new System.EventHandler(this.btn_transportesMarcados_Click);
+            // 
+            // cb_patinete
+            // 
+            this.cb_patinete.AutoSize = true;
+            this.cb_patinete.Location = new System.Drawing.Point(12, 144);
+            this.cb_patinete.Name = "cb_patinete";
+            this.cb_patinete.Size = new System.Drawing.Size(65, 17);
+            this.cb_patinete.TabIndex = 5;
+            this.cb_patinete.Text = "Patinete";
+            this.cb_patinete.UseVisualStyleBackColor = true;
+            this.cb_patinete.CheckedChanged += new System.EventHandler(this.cb_patinete_CheckedChanged);
+            // 
+            // F_CheckBox
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(335, 450);
+            this.Controls.Add(this.cb_patinete);
+            this.Controls.Add(this.btn_transportesMarcados);
+            this.Controls.Add(this.cb_onibus);
+            this.Controls.Add(this.cb_navio);
+            this.Controls.Add(this.cb_aviao);
+            this.Controls.Add(this.cb_carro);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "F_CheckBox";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "CheckBox";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox cb_carro;
+        private System.Windows.Forms.CheckBox cb_aviao;
+        private System.Windows.Forms.CheckBox cb_navio;
+        private System.Windows.Forms.CheckBox cb_onibus;
+        private System.Windows.Forms.Button btn_transportesMarcados;
+        private System.Windows.Forms.CheckBox cb_patinete;
+    }
+}
---
+++ Componentes/Componentes/Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace Componentes
+{
+    partial class F_Principal
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_adicionar = new System.Windows.Forms.Button();
+            this.tb_veiculo = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tb_listaVeiculos = new System.Windows.Forms.TextBox();
+            this.btn_limpar = new System.Windows.Forms.Button();
+            this.btn_mostrar = new System.Windows.Forms.Button();
+            this.btn_valNum = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.componentesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.checkboxToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.checkedListBoxToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btn_adicionar
+            // 
+            this.btn_adicionar.Location = new System.Drawing.Point(188, 40);
+            this.btn_adicionar.Name = "btn_adicionar";
+            this.btn_adicionar.Size = new System.Drawing.Size(75, 23);
+            this.btn_adicionar.TabIndex = 0;
+            this.btn_adicionar.Text = "Adicionar";
+            this.btn_adicionar.UseVisualStyleBackColor = true;
+            this.btn_adicionar.Click += new System.EventHandler(this.btn_adicionar_Click);
+            // 
+            // tb_veiculo
+            // 
+            this.tb_veiculo.Location = new System.Drawing.Point(12, 42);
+            this.tb_veiculo.Name = "tb_veiculo";
+            this.tb_veiculo.Size = new System.Drawing.Size(170, 20);
+            this.tb_veiculo.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(94, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Digite um Veículo:";
+            // 
+            // tb_listaVeiculos
+            // 
+            this.tb_listaVeiculos.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tb_listaVeiculos.Location = new System.Drawing.Point(12, 68);
+            this.tb_listaVeiculos.Multiline = true;
+            this.tb_listaVeiculos.Name = "tb_listaVeiculos";
+            this.tb_listaVeiculos.ReadOnly = true;
+            this.tb_listaVeiculos.Size = new System.Drawing.Size(251, 272);
+            this.tb_listaVeiculos.TabIndex = 3;
+            // 
+            // btn_limpar
+            // 
+            this.btn_limpar.Location = new System.Drawing.Point(12, 348);
+            this.btn_limpar.Name = "btn_limpar";
+            this.btn_limpar.Size = new System.Drawing.Size(251, 23);
+            this.btn_limpar.TabIndex = 4;
+            this.btn_limpar.Text = "Limpar";
+            this.btn_limpar.UseVisualStyleBackColor = true;
+            this.btn_limpar.Click += new System.EventHandler(this.btn_limpar_Click);
+            // 
+            // btn_mostrar
+            // 
+            this.btn_mostrar.Location = new System.Drawing.Point(12, 377);
+            this.btn_mostrar.Name = "btn_mostrar";
+            this.btn_mostrar.Size = new System.Drawing.Size(251, 23);
+            this.btn_mostrar.TabIndex = 5;
+            this.btn_mostrar.Text = "Mostrar Veículos";
+            this.btn_mostrar.UseVisualStyleBackColor = true;
+            this.btn_mostrar.Click += new System.EventHandler(this.btn_mostrar_Click);
+            // 
+            // btn_valNum
+            // 
+            this.btn_valNum.Location = new System.Drawing.Point(12, 407);
+            this.btn_valNum.Name = "btn_valNum";
+            this.btn_valNum.Size = new System.Drawing.Size(251, 23);
+            this.btn_valNum.TabIndex = 6;
+            this.btn_valNum.Text = "Valor da variável num";
+            this.btn_valNum.UseVisualStyleBackColor = true;
+            this.btn_valNum.Click += new System.EventHandler(this.btn_valNum_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.componentesToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(273, 24);
+            this.menuStrip1.TabIndex = 7;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // componentesToolStripMenuItem
+            // 
+            this.componentesToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.checkboxToolStripMenuItem,
+            this.checkedListBoxToolStripMenuItem});
+            this.componentesToolStripMenuItem.Name = "componentesToolStripMenuItem";
+            this.componentesToolStripMenuItem.Size = new System.Drawing.Size(94, 20);
+            this.componentesToolStripMenuItem.Text = "Componentes";
+            // 
+            // checkboxToolStripMenuItem
+            // 
+            this.checkboxToolStripMenuItem.Name = "checkboxToolStripMenuItem";
+            this.checkboxToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.checkboxToolStripMenuItem.Text = "Checkbox";
+            this.checkboxToolStripMenuItem.Click += new System.EventHandler(this.checkboxToolStripMenuItem_Click);
+            // 
+            // checkedListBoxToolStripMenuItem
+            // 
+            this.checkedListBoxToolStripMenuItem.Name = "checkedListBoxToolStripMenuItem";
+            this.checkedListBoxToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.checkedListBoxToolStripMenuItem.Text = "CheckedListBox";
+            this.checkedListBoxToolStripMenuItem.Click += new System.EventHandler(this.checkedListBoxToolStripMenuItem_Click);
+            // 
+            // F_Principal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(273, 438);
+            this.Controls.Add(this.btn_valNum);
+            this.Controls.Add(this.btn_mostrar);
+            this.Controls.Add(this.btn_limpar);
+            this.Controls.Add(this.tb_listaVeiculos);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tb_veiculo);
+            this.Controls.Add(this.btn_adicionar);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.Name = "F_Principal";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Componentes - CFB Cursos";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_adicionar;
+        private System.Windows.Forms.TextBox tb_veiculo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btn_limpar;
+        private System.Windows.Forms.Button btn_mostrar;
+        private System.Windows.Forms.Button btn_valNum;
+        public System.Windows.Forms.TextBox tb_listaVeiculos;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem componentesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem checkboxToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem checkedListBoxToolStripMenuItem;
+    }
+}
+
