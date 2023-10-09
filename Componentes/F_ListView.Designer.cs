cac2804a74a933ff1ca5295b04d73073a90ce582
Gianni Stephanie Carreira Honda <90318510+GianniHonda@users.noreply.github.com>
2023-10-10 08:29:01 +0900

    Continued (ListView)

--- Componentes/F_ListView.Designer.cs
+++ Componentes/F_ListView.Designer.cs
@@ -28,18 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.ListViewItem listViewItem1 = new System.Windows.Forms.ListViewItem(new string[] {
+            System.Windows.Forms.ListViewItem listViewItem7 = new System.Windows.Forms.ListViewItem(new string[] {
             "0",
             "Mouse",
             "120",
             "19,99"}, -1);
-            System.Windows.Forms.ListViewItem listViewItem2 = new System.Windows.Forms.ListViewItem(new string[] {
-            "",
+            System.Windows.Forms.ListViewItem listViewItem8 = new System.Windows.Forms.ListViewItem(new string[] {
+            "1",
             "Teclado",
             "85",
             "39,99"}, -1);
-            System.Windows.Forms.ListViewItem listViewItem3 = new System.Windows.Forms.ListViewItem(new string[] {
-            "",
+            System.Windows.Forms.ListViewItem listViewItem9 = new System.Windows.Forms.ListViewItem(new string[] {
+            "2",
             "Monitor",
             "38",
             "500,00"}, -1);
@@ -48,6 +48,17 @@
             this.col_produto = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.col_qtde = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.col_preco = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.tb_id = new System.Windows.Forms.TextBox();
+            this.tb_produto = new System.Windows.Forms.TextBox();
+            this.tb_qtde = new System.Windows.Forms.TextBox();
+            this.tb_preco = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btn_adicionar = new System.Windows.Forms.Button();
+            this.btn_remover = new System.Windows.Forms.Button();
+            this.btn_obter = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // lv_produtos
@@ -59,16 +70,16 @@
             this.col_preco});
             this.lv_produtos.FullRowSelect = true;
             this.lv_produtos.HideSelection = false;
-            listViewItem2.IndentCount = 1;
-            listViewItem3.IndentCount = 2;
+            listViewItem8.IndentCount = 1;
+            listViewItem9.IndentCount = 2;
             this.lv_produtos.Items.AddRange(new System.Windows.Forms.ListViewItem[] {
-            listViewItem1,
-            listViewItem2,
-            listViewItem3});
+            listViewItem7,
+            listViewItem8,
+            listViewItem9});
             this.lv_produtos.Location = new System.Drawing.Point(13, 13);
             this.lv_produtos.MultiSelect = false;
             this.lv_produtos.Name = "lv_produtos";
-            this.lv_produtos.Size = new System.Drawing.Size(424, 124);
+            this.lv_produtos.Size = new System.Drawing.Size(442, 124);
             this.lv_produtos.TabIndex = 0;
             this.lv_produtos.UseCompatibleStateImageBehavior = false;
             this.lv_produtos.View = System.Windows.Forms.View.Details;
@@ -93,17 +104,123 @@
             this.col_preco.Text = "Preço";
             this.col_preco.Width = 120;
             // 
+            // tb_id
+            // 
+            this.tb_id.Location = new System.Drawing.Point(13, 204);
+            this.tb_id.Name = "tb_id";
+            this.tb_id.Size = new System.Drawing.Size(39, 20);
+            this.tb_id.TabIndex = 1;
+            // 
+            // tb_produto
+            // 
+            this.tb_produto.Location = new System.Drawing.Point(58, 204);
+            this.tb_produto.Name = "tb_produto";
+            this.tb_produto.Size = new System.Drawing.Size(247, 20);
+            this.tb_produto.TabIndex = 2;
+            // 
+            // tb_qtde
+            // 
+            this.tb_qtde.Location = new System.Drawing.Point(311, 204);
+            this.tb_qtde.Name = "tb_qtde";
+            this.tb_qtde.Size = new System.Drawing.Size(63, 20);
+            this.tb_qtde.TabIndex = 3;
+            // 
+            // tb_preco
+            // 
+            this.tb_preco.Location = new System.Drawing.Point(380, 204);
+            this.tb_preco.Name = "tb_preco";
+            this.tb_preco.Size = new System.Drawing.Size(75, 20);
+            this.tb_preco.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 185);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(18, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "ID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(58, 185);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Produto";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(312, 185);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Quantidade";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(381, 185);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Preço";
+            // 
+            // btn_adicionar
+            // 
+            this.btn_adicionar.Location = new System.Drawing.Point(16, 248);
+            this.btn_adicionar.Name = "btn_adicionar";
+            this.btn_adicionar.Size = new System.Drawing.Size(135, 23);
+            this.btn_adicionar.TabIndex = 9;
+            this.btn_adicionar.Text = "Adicionar";
+            this.btn_adicionar.UseVisualStyleBackColor = true;
+            this.btn_adicionar.Click += new System.EventHandler(this.btn_adicionar_Click);
+            // 
+            // btn_remover
+            // 
+            this.btn_remover.Location = new System.Drawing.Point(16, 278);
+            this.btn_remover.Name = "btn_remover";
+            this.btn_remover.Size = new System.Drawing.Size(135, 23);
+            this.btn_remover.TabIndex = 10;
+            this.btn_remover.Text = "Remover";
+            this.btn_remover.UseVisualStyleBackColor = true;
+            this.btn_remover.Click += new System.EventHandler(this.btn_remover_Click);
+            // 
+            // btn_obter
+            // 
+            this.btn_obter.Location = new System.Drawing.Point(16, 308);
+            this.btn_obter.Name = "btn_obter";
+            this.btn_obter.Size = new System.Drawing.Size(135, 23);
+            this.btn_obter.TabIndex = 11;
+            this.btn_obter.Text = "Obter";
+            this.btn_obter.UseVisualStyleBackColor = true;
+            this.btn_obter.Click += new System.EventHandler(this.btn_obter_Click);
+            // 
             // F_ListView
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(456, 450);
+            this.Controls.Add(this.btn_obter);
+            this.Controls.Add(this.btn_remover);
+            this.Controls.Add(this.btn_adicionar);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tb_preco);
+            this.Controls.Add(this.tb_qtde);
+            this.Controls.Add(this.tb_produto);
+            this.Controls.Add(this.tb_id);
             this.Controls.Add(this.lv_produtos);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.Name = "F_ListView";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "F_ListView";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -114,5 +231,16 @@
         private System.Windows.Forms.ColumnHeader col_produto;
         private System.Windows.Forms.ColumnHeader col_qtde;
         private System.Windows.Forms.ColumnHeader col_preco;
+        private System.Windows.Forms.TextBox tb_id;
+        private System.Windows.Forms.TextBox tb_produto;
+        private System.Windows.Forms.TextBox tb_qtde;
+        private System.Windows.Forms.TextBox tb_preco;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btn_adicionar;
+        private System.Windows.Forms.Button btn_remover;
+        private System.Windows.Forms.Button btn_obter;
     }
 }
--- Componentes/F_ListView.cs
+++ Componentes/F_ListView.cs
@@ -16,5 +16,65 @@ namespace Componentes
         {
             InitializeComponent();
         }
+
+        private void limpar()
+        {
+            tb_id.Clear();
+            tb_produto.Clear();
+            tb_qtde.Clear();
+            tb_preco.Clear();
+            tb_id.Focus();
+        }
+
+        private void btn_adicionar_Click(object sender, EventArgs e)
+        {
+            if (tb_id.Text == "")
+            {
+                MessageBox.Show("ID não pode ser vazio");
+                tb_id.Focus();
+                return;
+            }
+            if (tb_produto.Text == "")
+            {
+                MessageBox.Show("Produto não pode ser vazio");
+                tb_produto.Focus();
+                return;
+            }
+            if (tb_qtde.Text == "")
+            {
+                MessageBox.Show("Quantidade não pode ser vazio");
+                tb_qtde.Focus();
+                return;
+            }
+            if (tb_preco.Text == "")
+            {
+                MessageBox.Show("Preço não pode ser vazio");
+                tb_preco.Focus();
+                return;
+            }
+
+            string[] pr = new string[4];
+            pr[0] = tb_id.Text;
+            pr[1] = tb_produto.Text;
+            pr[2] = tb_qtde.Text;
+            pr[3] = tb_preco.Text;
+
+            ListViewItem l=new ListViewItem(pr);
+            lv_produtos.Items.Add(l);
+            limpar();
+        }
+
+        private void btn_remover_Click(object sender, EventArgs e)
+        {
+            lv_produtos.Items.RemoveAt(lv_produtos.SelectedIndices[0]);
+        }
+
+        private void btn_obter_Click(object sender, EventArgs e)
+        {
+            tb_id.Text = lv_produtos.SelectedItems[0].SubItems[0].Text;
+            tb_produto.Text = lv_produtos.SelectedItems[0].SubItems[1].Text;
+            tb_qtde.Text = lv_produtos.SelectedItems[0].SubItems[2].Text;
+            tb_preco.Text = lv_produtos.SelectedItems[0].SubItems[3].Text;
+        }
     }
 }
