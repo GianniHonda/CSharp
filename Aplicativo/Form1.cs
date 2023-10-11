44896d7bce5e68bc3fd0246521d553858d527cde
Gianni Stephanie Carreira Honda <90318510+GianniHonda@users.noreply.github.com>
2023-10-11 11:36:13 +0900

    Continued text editor app
    In this part I set the program to open a saved file.

---
+++ Aplicativo/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Aplicativo
+{
+    public partial class Form1 : Form
+    {
+
+        StringReader leitura = null;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Novo()
+        {
+            richTextBox1.Clear();
+            richTextBox1.Focus();
+        }
+
+        private void btn_novo_Click(object sender, EventArgs e)
+        {
+            Novo();  
+        }
+
+        private void novoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Novo();
+        }
+
+        private void Salvar()
+        {
+            try
+            {
+                if (this.saveFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    FileStream arquivo = new FileStream(saveFileDialog1.FileName, FileMode.OpenOrCreate, FileAccess.Write);
+                    StreamWriter cfb_streamWriter = new StreamWriter(arquivo);
+                    cfb_streamWriter.Flush();
+                    cfb_streamWriter.BaseStream.Seek(0, SeekOrigin.Begin);
+                    cfb_streamWriter.Write(this.richTextBox1.Text);
+                    cfb_streamWriter.Flush();
+                    cfb_streamWriter.Close();
+                }
+            }catch(Exception ex)
+            {
+                MessageBox.Show("Erro na gravação: " + ex.Message,"Erro ao gravar",MessageBoxButtons.OK,MessageBoxIcon.Error);
+            }
+        }
+
+        private void btn_salvar_Click(object sender, EventArgs e)
+        {
+            Salvar();
+        }
+
+        private void salvarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Salvar();
+        }
+
+        private void Abrir()
+        {
+            this.openFileDialog1.Title = "Abrir Arquivo";
+            openFileDialog1.InitialDirectory = @"D:\\C#\\Aulas\\Parte2\\Aplicativo\";
+            openFileDialog1.Filter="(*.CFB)|*.CFB";
+            DialogResult dr=this.openFileDialog1.ShowDialog();
+            if(dr == System.Windows.Forms.DialogResult.OK) 
+            {
+                try
+                {
+                    FileStream arquivo = new FileStream(openFileDialog1.FileName,FileMode.Open,FileAccess.Read);
+                    StreamReader cfb_streamReader =new StreamReader(arquivo);
+                    cfb_streamReader.BaseStream.Seek(0, SeekOrigin.Begin);
+                    this.richTextBox1.Text = "";
+                    string linha = cfb_streamReader.ReadLine();
+                    while( linha != null )
+                    {
+                        this.richTextBox1.Text += linha + "\n";
+                        linha = cfb_streamReader.ReadLine();
+                    }
+                    cfb_streamReader.Close();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Erro de leitura: " + ex.Message, "Erro ao ker", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void btn_abrir_Click(object sender, EventArgs e)
+        {
+            Abrir();
+        }
+
+        private void abrirToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Abrir();
+        }
+    }
+}
