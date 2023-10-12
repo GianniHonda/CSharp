0eaff32347d041aef8580ffc4bdf14221bcb6fdc
Gianni Stephanie Carreira Honda <90318510+GianniHonda@users.noreply.github.com>
2023-10-13 08:44:34 +0900

    TabControl

---
+++ Componentes/F_TabControl.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace Componentes
+{
+    partial class F_TabControl
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tab_componentes = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tb_nomeNovaTab = new System.Windows.Forms.TextBox();
+            this.btn_novaTab = new System.Windows.Forms.Button();
+            this.btn_removerTabAtual = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.btn_posicionarTab = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tabControl1.SuspendLayout();
+            this.tab_componentes.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tab_componentes);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(12, 68);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(580, 370);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tab_componentes
+            // 
+            this.tab_componentes.Controls.Add(this.button1);
+            this.tab_componentes.Controls.Add(this.textBox1);
+            this.tab_componentes.Controls.Add(this.label1);
+            this.tab_componentes.Location = new System.Drawing.Point(4, 22);
+            this.tab_componentes.Name = "tab_componentes";
+            this.tab_componentes.Padding = new System.Windows.Forms.Padding(3);
+            this.tab_componentes.Size = new System.Drawing.Size(572, 344);
+            this.tab_componentes.TabIndex = 0;
+            this.tab_componentes.Text = "Componentes";
+            this.tab_componentes.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(192, 74);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // tb_nomeNovaTab
+            // 
+            this.tb_nomeNovaTab.Location = new System.Drawing.Point(16, 13);
+            this.tb_nomeNovaTab.Name = "tb_nomeNovaTab";
+            this.tb_nomeNovaTab.Size = new System.Drawing.Size(100, 20);
+            this.tb_nomeNovaTab.TabIndex = 1;
+            // 
+            // btn_novaTab
+            // 
+            this.btn_novaTab.Location = new System.Drawing.Point(123, 9);
+            this.btn_novaTab.Name = "btn_novaTab";
+            this.btn_novaTab.Size = new System.Drawing.Size(192, 23);
+            this.btn_novaTab.TabIndex = 2;
+            this.btn_novaTab.Text = "Nova Tab";
+            this.btn_novaTab.UseVisualStyleBackColor = true;
+            this.btn_novaTab.Click += new System.EventHandler(this.btn_novaTab_Click);
+            // 
+            // btn_removerTabAtual
+            // 
+            this.btn_removerTabAtual.Location = new System.Drawing.Point(321, 9);
+            this.btn_removerTabAtual.Name = "btn_removerTabAtual";
+            this.btn_removerTabAtual.Size = new System.Drawing.Size(192, 23);
+            this.btn_removerTabAtual.TabIndex = 3;
+            this.btn_removerTabAtual.Text = "Remover Tab Atual";
+            this.btn_removerTabAtual.UseVisualStyleBackColor = true;
+            this.btn_removerTabAtual.Click += new System.EventHandler(this.btn_removerTabAtual_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(16, 39);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(100, 20);
+            this.numericUpDown1.TabIndex = 4;
+            // 
+            // btn_posicionarTab
+            // 
+            this.btn_posicionarTab.Location = new System.Drawing.Point(123, 39);
+            this.btn_posicionarTab.Name = "btn_posicionarTab";
+            this.btn_posicionarTab.Size = new System.Drawing.Size(192, 23);
+            this.btn_posicionarTab.TabIndex = 5;
+            this.btn_posicionarTab.Text = "Posicionar na Tab";
+            this.btn_posicionarTab.UseVisualStyleBackColor = true;
+            this.btn_posicionarTab.Click += new System.EventHandler(this.btn_posicionarTab_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(18, 30);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(18, 57);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // F_TabControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(604, 450);
+            this.Controls.Add(this.btn_posicionarTab);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.btn_removerTabAtual);
+            this.Controls.Add(this.btn_novaTab);
+            this.Controls.Add(this.tb_nomeNovaTab);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "F_TabControl";
+            this.Text = "F_TabControl";
+            this.Load += new System.EventHandler(this.F_TabControl_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tab_componentes.ResumeLayout(false);
+            this.tab_componentes.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tab_componentes;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TextBox tb_nomeNovaTab;
+        private System.Windows.Forms.Button btn_novaTab;
+        private System.Windows.Forms.Button btn_removerTabAtual;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button btn_posicionarTab;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ Componentes/F_TabControl.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Componentes
+{
+    public partial class F_TabControl : Form
+    {
+        public F_TabControl()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_novaTab_Click(object sender, EventArgs e)
+        {
+            if (tb_nomeNovaTab.Text != "")
+            {
+                TabPage pagina = new TabPage();
+                pagina.Text = tb_nomeNovaTab.Text;
+                pagina.Name = tb_nomeNovaTab.Text;
+                pagina.TabIndex = tabControl1.TabPages.Count;
+
+                tabControl1.TabPages.Add(pagina);
+                tb_nomeNovaTab.Clear();
+                definirMaximo();
+            }
+            else
+            {
+                MessageBox.Show("Digite o nome da nova Tab");
+                tb_nomeNovaTab.Focus();
+            }
+        }
+
+        private void btn_removerTabAtual_Click(object sender, EventArgs e)
+        {
+            tabControl1.TabPages.Remove(tabControl1.SelectedTab);
+            definirMaximo();
+        }
+
+        private void btn_posicionarTab_Click(object sender, EventArgs e)
+        {
+            //if (numericUpDown1.Value < tabControl1.TabPages.Count)
+            //{
+                tabControl1.SelectedIndex = Int32.Parse(numericUpDown1.Value.ToString());
+            //}
+        }
+
+        private void definirMaximo()
+        {
+            numericUpDown1.Maximum = tabControl1.TabPages.Count-1;
+        }
+
+        private void F_TabControl_Load(object sender, EventArgs e)
+        {
+            definirMaximo();
+        }
+    }
+}
