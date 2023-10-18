1d0a370c608ae3cc2600680808841de2c711f481
Gianni Stephanie Carreira Honda <90318510+GianniHonda@users.noreply.github.com>
2023-10-19 08:34:07 +0900

    Continued gym app
    Creation and configuration of the Teacher Management form, with integration to the database.

---
+++ F_GestaoProfessores.Designer.cs
@@ -0,0 +1,221 @@
+﻿namespace CFB_Academia
+{
+    partial class F_GestaoProfessores
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btn_fechar = new System.Windows.Forms.Button();
+            this.btn_excluir = new System.Windows.Forms.Button();
+            this.btn_salvar = new System.Windows.Forms.Button();
+            this.btn_novoProfessor = new System.Windows.Forms.Button();
+            this.dgv_professores = new System.Windows.Forms.DataGridView();
+            this.tb_id = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tb_nome = new System.Windows.Forms.TextBox();
+            this.mtb_telefone = new System.Windows.Forms.MaskedTextBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_professores)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btn_fechar);
+            this.panel1.Controls.Add(this.btn_excluir);
+            this.panel1.Controls.Add(this.btn_salvar);
+            this.panel1.Controls.Add(this.btn_novoProfessor);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 419);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(396, 31);
+            this.panel1.TabIndex = 11;
+            // 
+            // btn_fechar
+            // 
+            this.btn_fechar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_fechar.Location = new System.Drawing.Point(274, 3);
+            this.btn_fechar.Name = "btn_fechar";
+            this.btn_fechar.Size = new System.Drawing.Size(75, 23);
+            this.btn_fechar.TabIndex = 3;
+            this.btn_fechar.Text = "Fechar";
+            this.btn_fechar.UseVisualStyleBackColor = true;
+            this.btn_fechar.Click += new System.EventHandler(this.btn_fechar_Click_1);
+            // 
+            // btn_excluir
+            // 
+            this.btn_excluir.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_excluir.Location = new System.Drawing.Point(189, 3);
+            this.btn_excluir.Name = "btn_excluir";
+            this.btn_excluir.Size = new System.Drawing.Size(79, 23);
+            this.btn_excluir.TabIndex = 2;
+            this.btn_excluir.Text = "Excluir";
+            this.btn_excluir.UseVisualStyleBackColor = true;
+            this.btn_excluir.Click += new System.EventHandler(this.btn_excluir_Click);
+            // 
+            // btn_salvar
+            // 
+            this.btn_salvar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_salvar.Location = new System.Drawing.Point(110, 3);
+            this.btn_salvar.Name = "btn_salvar";
+            this.btn_salvar.Size = new System.Drawing.Size(73, 23);
+            this.btn_salvar.TabIndex = 1;
+            this.btn_salvar.Text = "Salvar ";
+            this.btn_salvar.UseVisualStyleBackColor = true;
+            this.btn_salvar.Click += new System.EventHandler(this.btn_salvar_Click_1);
+            // 
+            // btn_novoProfessor
+            // 
+            this.btn_novoProfessor.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_novoProfessor.Location = new System.Drawing.Point(16, 3);
+            this.btn_novoProfessor.Name = "btn_novoProfessor";
+            this.btn_novoProfessor.Size = new System.Drawing.Size(88, 23);
+            this.btn_novoProfessor.TabIndex = 0;
+            this.btn_novoProfessor.Text = "Novo Professor";
+            this.btn_novoProfessor.UseVisualStyleBackColor = true;
+            this.btn_novoProfessor.Click += new System.EventHandler(this.btn_novoProfessor_Click_1);
+            // 
+            // dgv_professores
+            // 
+            this.dgv_professores.AllowUserToAddRows = false;
+            this.dgv_professores.AllowUserToDeleteRows = false;
+            dataGridViewCellStyle4.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle4.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle4.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle4.SelectionBackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle4.SelectionForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle4.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgv_professores.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle4;
+            this.dgv_professores.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv_professores.EnableHeadersVisualStyles = false;
+            this.dgv_professores.Location = new System.Drawing.Point(16, 50);
+            this.dgv_professores.MultiSelect = false;
+            this.dgv_professores.Name = "dgv_professores";
+            this.dgv_professores.ReadOnly = true;
+            this.dgv_professores.RowHeadersVisible = false;
+            this.dgv_professores.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgv_professores.Size = new System.Drawing.Size(368, 359);
+            this.dgv_professores.TabIndex = 10;
+            this.dgv_professores.SelectionChanged += new System.EventHandler(this.dgv_professores_SelectionChanged_1);
+            // 
+            // tb_id
+            // 
+            this.tb_id.Location = new System.Drawing.Point(16, 24);
+            this.tb_id.Name = "tb_id";
+            this.tb_id.ReadOnly = true;
+            this.tb_id.Size = new System.Drawing.Size(52, 20);
+            this.tb_id.TabIndex = 8;
+            this.tb_id.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(71, 7);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Professor";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 7);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(18, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "ID";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(284, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 13);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Telefone";
+            // 
+            // tb_nome
+            // 
+            this.tb_nome.Location = new System.Drawing.Point(75, 24);
+            this.tb_nome.Name = "tb_nome";
+            this.tb_nome.Size = new System.Drawing.Size(203, 20);
+            this.tb_nome.TabIndex = 13;
+            // 
+            // mtb_telefone
+            // 
+            this.mtb_telefone.Location = new System.Drawing.Point(284, 24);
+            this.mtb_telefone.Mask = "(00) 0000-0000";
+            this.mtb_telefone.Name = "mtb_telefone";
+            this.mtb_telefone.Size = new System.Drawing.Size(100, 20);
+            this.mtb_telefone.TabIndex = 14;
+            // 
+            // F_GestaoProfessores
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(396, 450);
+            this.Controls.Add(this.mtb_telefone);
+            this.Controls.Add(this.tb_nome);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.dgv_professores);
+            this.Controls.Add(this.tb_id);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "F_GestaoProfessores";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "7";
+            this.Load += new System.EventHandler(this.F_GestaoProfessores_Load_1);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_professores)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btn_fechar;
+        private System.Windows.Forms.Button btn_excluir;
+        private System.Windows.Forms.Button btn_salvar;
+        private System.Windows.Forms.Button btn_novoProfessor;
+        private System.Windows.Forms.DataGridView dgv_professores;
+        private System.Windows.Forms.TextBox tb_id;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tb_nome;
+        private System.Windows.Forms.MaskedTextBox mtb_telefone;
+    }
+}
---
+++ F_GestaoProfessores.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CFB_Academia
+{
+    public partial class F_GestaoProfessores : Form
+    {
+        public F_GestaoProfessores()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_excluir_Click(object sender, EventArgs e)
+        {
+            DialogResult res = MessageBox.Show("Confirma exclusão?", "Excluir?", MessageBoxButtons.YesNo);
+            if (res == DialogResult.Yes)
+            {
+                string vquery = "DELETE FROM tb_professores WHERE N_IDPROFESSOR=" + tb_id.Text;
+                Banco.dml(vquery);
+                dgv_professores.Rows.Remove(dgv_professores.CurrentRow);
+            }
+        }
+
+        private void F_GestaoProfessores_Load_1(object sender, EventArgs e)
+        {
+            string vquery = @"
+                SELECT
+                    N_IDPROFESSOR as 'ID',
+                    T_NOMEPROFESSOR as 'Professor',
+                    T_TELEFONE as 'Telefone'
+                FROM
+                    tb_professores
+                ORDER BY
+                    N_IDPROFESSOR
+            ";
+            dgv_professores.DataSource = Banco.dql(vquery);
+            dgv_professores.Columns[0].Width = 60;
+            dgv_professores.Columns[1].Width = 170;
+            dgv_professores.Columns[2].Width = 100;
+        }
+
+        private void dgv_professores_SelectionChanged_1(object sender, EventArgs e)
+        {
+            DataGridView dgv = (DataGridView)sender;
+            int contLinhas = dgv.SelectedRows.Count;
+            if (contLinhas > 0)
+            {
+                DataTable dt = new DataTable();
+                string vid = dgv.SelectedRows[0].Cells[0].Value.ToString();
+                string vquery = @"
+                    SELECT
+                        *
+                    FROM
+                        tb_professores
+                    WHERE
+                        N_IDPROFESSOR=" + vid;
+                dt = Banco.dql(vquery);
+                tb_id.Text = dt.Rows[0].Field<Int64>("N_IDPROFESSOR").ToString();
+                tb_nome.Text = dt.Rows[0].Field<string>("T_NOMEPROFESSOR");
+                mtb_telefone.Text = dt.Rows[0].Field<string>("T_TELEFONE");
+            }
+        }
+
+        private void btn_novoProfessor_Click_1(object sender, EventArgs e)
+        {
+            tb_id.Clear();
+            tb_nome.Clear();
+            mtb_telefone.Focus();
+            tb_nome.Focus();
+        }
+
+        private void btn_salvar_Click_1(object sender, EventArgs e)
+        {
+            string vquery;
+            if (tb_id.Text == "")
+            {
+                vquery = "INSERT INTO tb_professores (T_NOMEPROFESSOR, T_TELEFONE) VALUES ('" + tb_nome.Text + "','" + mtb_telefone.Text + "')";
+            }
+            else
+            {
+                vquery = "'UPDATE tb_professores SET T_NOMEPROFESSOR'" + tb_nome.Text + "', T_TELEFONE='" + mtb_telefone.Text + "'WHERE N_IDPROFESSOR='" + tb_id.Text;
+            }
+            Banco.dml(vquery);
+            vquery = @"
+                SELECT
+                    N_IDPROFESSOR as 'ID',
+                    T_NOMEPROFESSOR as 'Professor',
+                    T_TELEFONE as 'Telefone'
+                FROM
+                    tb_professores
+                ORDER BY
+                    N_IDPROFESSOR
+            ";
+            dgv_professores.DataSource = Banco.dql(vquery);
+        }
+
+        private void btn_fechar_Click_1(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
