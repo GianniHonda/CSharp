715f47ef76ce79e388656ca1b1170d896287e894
Gianni Stephanie Carreira Honda <90318510+GianniHonda@users.noreply.github.com>
2023-11-29 10:16:33 +0900

    Calculator App
    Simple creation of a calculator with basic addition, subtraction, multiplication and division operations.

---
+++ App_Calculadora/Calculadora/Calculadora/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Qual operação deseja fazer:");
+            Console.WriteLine("1 - Adição");
+            Console.WriteLine("2 - Subtração");
+            Console.WriteLine("3 - Multiplicação");
+            Console.WriteLine("4 - Divisão \n");
+
+            int operacao = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o primeiro número: ");
+            int num1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o segundo número: ");
+            int num2 = int.Parse(Console.ReadLine());
+
+            int resultado = 0;
+
+            switch(operacao)
+            {
+                case 1:
+                    {
+                        resultado = Adicao(num1, num2);
+                        break;
+                    }
+                case 2:
+                    {
+                        resultado = Subtracao(num1, num2);
+                        break;
+                    }
+                case 3:
+                    {
+                        resultado = Multiplicacao(num1, num2);
+                        break;
+                    }
+                case 4:
+                    {
+                        resultado = Divisao(num1, num2);
+                        break;
+                    }
+
+                default:
+                    Console.WriteLine("Número iinválido, digite outro número");
+                    break;
+            }
+
+            Console.WriteLine("O resultado da operação com os números {0} e {1} é: {2}", num1, num2, resultado);
+
+            Console.ReadLine() ;
+        }
+
+
+
+
+        public static int Adicao (int numero1, int numero2)
+        {
+            int result = numero1 + numero2;
+            return result;
+        }
+
+        public static int Subtracao(int numero1, int numero2)
+        {
+            int result = numero1 - numero2;
+            return result;
+        }
+
+        public static int Multiplicacao(int numero1, int numero2)
+        {
+            int result = numero1 * numero2;
+            return result;
+        }
+
+        public static int Divisao(int numero1, int numero2)
+        {
+            int result = numero1 / numero2;
+            return result;
+        }
+    }
+}
